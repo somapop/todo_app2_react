feb9879ac67b8c7b35ebd00c5d0e700b796149f5
David Scattergood <davescattz@hotmail.com>
2019-11-12 22:40:52 +0000

    Updates

---
+++ precache-manifest.8f0b77c116cc46fcd5ce0b348e216726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f052b9e476da163657ea844c2eb43618",
+    "url": "/todo_app2_react/index.html"
+  },
+  {
+    "revision": "37552ba83ca2f6b688c0",
+    "url": "/todo_app2_react/static/css/main.0176b833.chunk.css"
+  },
+  {
+    "revision": "22a804d6dd81d852b326",
+    "url": "/todo_app2_react/static/js/2.d786813d.chunk.js"
+  },
+  {
+    "revision": "37552ba83ca2f6b688c0",
+    "url": "/todo_app2_react/static/js/main.75439300.chunk.js"
+  },
+  {
+    "revision": "60847691e3766b80d05d",
+    "url": "/todo_app2_react/static/js/runtime-main.dbedd5e7.js"
+  }
+]);
