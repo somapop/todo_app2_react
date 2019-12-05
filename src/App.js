24d75483ee6a40294ef24703e0ad579e14c430c5
David Scattergood <davescattz@hotmail.com>
2019-12-05 11:43:43 +0000

    axios additions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import uuid from "uuid/v4";
+import axios from "axios";
 import moment from "moment";
 import logo from './images/tasq_logo_6.png';
 import Header from "./components/Header";
@@ -13,14 +14,29 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    tasks: [
-      { text: "Book Flights", completed: true, date: "2019-10-21", id: uuid(), dueBy: "2019-11-18" },
-      { text: "Wash The Car", completed: false, date: "2019-10-23", id: uuid(), dueBy: "2019-11-13" },
-      { text: "Book Gym Session", completed: true, date: "2019-10-25", id: uuid(), dueBy: "2019-10-18" },
-      { text: "Purchase Fruit", completed: true, date: "2019-10-25", id: uuid(), dueBy: "2019-11-19" },
-      { text: "Organise Vets", completed: false, date: "2019-10-26", id: uuid(), dueBy: "2019-11-22" },
-    ]
+    tasks: []
+      // { text: "Book Flights", completed: true, date: "2019-10-21", id: uuid(), dueBy: "2019-11-18" },
+      // { text: "Wash The Car", completed: false, date: "2019-10-23", id: uuid(), dueBy: "2019-11-13" },
+      // { text: "Book Gym Session", completed: true, date: "2019-10-25", id: uuid(), dueBy: "2019-10-18" },
+      // { text: "Purchase Fruit", completed: true, date: "2019-10-25", id: uuid(), dueBy: "2019-11-19" },
+      // { text: "Organise Vets", completed: false, date: "2019-10-26", id: uuid(), dueBy: "2019-11-22" },
+    
   };
+componentDidMount() {
+  axios.get("https://r8cberekcc.execute-api.eu-west-1.amazonaws.com/dev/tasks")
+  .then((response) => {
+    const tasks = response.data;
+    this.setState({
+      tasks: tasks
+    })
+ 
+  })
+  .catch((err) => {
+console.log("Error getting taks data", err);
+})
+}
+
+
   //This func should update the state with a new task
   addNewTask = (taskText, dueByDate) => {
     const tasksCopy = this.state.tasks.slice();
@@ -28,30 +44,53 @@ class App extends Component {
     const newTask = {
       text: taskText,
       completed: false,
-      date: moment().format("YYYY-MM-DD"),
-      id: uuid(),
-      dueBy: dueByDate
+      dateCreated: moment().format("YYYY-MM-DD"),
+      // id: uuid(),
+      dateDue: dueByDate,
+      userId: 1
     };
 
-    tasksCopy.push(newTask)
-    this.setState({
-      tasks: tasksCopy
-    });
-  }
+
+
+    axios.post("https://r8cberekcc.execute-api.eu-west-1.amazonaws.com/dev/tasks", newTask)
+      .then((response) => {
+        const taskFromDB = response.data;
+         tasksCopy.push(taskFromDB)
+
+         this.setState({
+         tasks: tasksCopy
+      });
+      })  
+
+     .catch((err) => {
+          console.log("Error creating task", err);
+        });
+
+  };
 
   deleteTask = id => {
-    const filteredTasks = this.state.tasks.filter(task => {
-      return task.id !== id
+
+    axios.delete("https://r8cberekcc.execute-api.eu-west-1.amazonaws.com/dev/tasks/" + id)
+      .then((tasks) => {
+        const filteredTasks = this.state.tasks.filter(task => {
+      return task.taskId !== id
     });
 
-    this.setState({
+      this.setState({
       tasks: filteredTasks
-    });
-  }
+      });
+    })
+           
+      .catch((err) => {  
+    console.log(err);
+  })
+}
+
+ 
 
   completeTask = id => {
     const updatedTasks = this.state.tasks.map(task => {
-      if (task.id === id) {
+      if (task.taskId === id) {
         task.completed = true;
       }
 
@@ -75,7 +114,7 @@ class App extends Component {
     const styles = {
       padding: 20
     }
-
+  
 
     return (
 
@@ -91,7 +130,7 @@ class App extends Component {
         <h2 style={styles}>TASQs 2DO</h2>
         {incompleteTasks.map(task => {
           return (
-            <Item text={task.text} completed={task.completed} key={task.id} deleteTaskFunc={this.deleteTask} id={task.id} completeTaskFunc={this.completeTask} date={task.date} dueBy={task.dueBy} />
+            <Item text={task.text} completed={task.completed} key={task.taskId} deleteTaskFunc={this.deleteTask} id={task.id} completeTaskFunc={this.completeTask} date={task.dateCreated} dueBy={task.dateDue} />
           );
         })}
 
@@ -99,7 +138,7 @@ class App extends Component {
 
         {completedTasks.map(task => {
           return (
-            <Item text={task.text} completed={task.completed} key={task.id} deleteTaskFunc={this.deleteTask} id={task.id} date={task.date} dueBy={task.dueBy} />
+            <Item text={task.text} completed={task.completed} key={task.id} deleteTaskFunc={this.deleteTask} id={task.id} date={task.dateCreated} dueBy={task.dateDue} />
           );
         })}
       </div>
